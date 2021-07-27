4f048776bb9ab671bcf277cd6a579a82646b1823
MCQAQ <215105350@qq.com>
2021-07-27 08:02:23 +0800

    20210727

--- demo/src/main/java/com/sbstest/Service/RedisServiceImpl.java
+++ demo/src/main/java/com/sbstest/Service/RedisServiceImpl.java
@@ -1,8 +1,16 @@
 package com.sbstest.Service;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
@@ -21,7 +29,12 @@ public class RedisServiceImpl {
     public boolean set(final String key, Object value, Long expireTime) {
         boolean result = false;
         try {
-
+            RedisSerializer stringSerializer = new StringRedisSerializer();
+            redisTemplate.setKeySerializer(stringSerializer);
+            redisTemplate.setValueSerializer(stringSerializer);
+            redisTemplate.setHashKeySerializer(stringSerializer);
+            redisTemplate.setHashValueSerializer(stringSerializer);
+            this.redisTemplate = redisTemplate;
             ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
             operations.set(key,value);
             redisTemplate.expire(key, expireTime, TimeUnit.MINUTES);
