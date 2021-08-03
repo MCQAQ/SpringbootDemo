9d6edc46519618a8e36930865fa39ac39ebd15f6
liutianqi <liutianqi@cibfintech.com>
2021-08-03 20:52:46 +0800

    20210803

---
+++ demo/springcloud_client/src/main/java/com/example/springcloud_client/controller/HelloClientController.java
@@ -0,0 +1,24 @@
+package com.example.springcloud_client.controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by 21510 on 2021/8/3.
+ */
+
+@RestController
+public class HelloClientController {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8000";
+
+    @RequestMapping(value = "/Hello",method = RequestMethod.GET)
+    public String getHello(){
+        RestTemplate template = new RestTemplate();
+        return template.getForObject(REST_URL_PREFIX+"/hello",String.class);
+    }
+
+}
